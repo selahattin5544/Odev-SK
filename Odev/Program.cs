5a1b1d0e84303db4542ed16690ce387fface60fc
SELAHATTIN KARAYEL <karayelselahattin1@gmail.com>
2021-04-23 00:26:06 +0300

    Düzenledim ve bitirdim.

--- Odev/Program.cs
+++ Odev/Program.cs
@@ -13,8 +13,16 @@ namespace Odev
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("altın       gümüş     platin      paladyum");
+            Console.WriteLine("Atın(KG/TL)       Gümüş(KG/TL)    Platin(KG/TL)      Paladyum(KG/TL)");
+            int sayac=0;
         A:
+            if (sayac==10)
+            {
+                Console.Clear();
+                Console.WriteLine("Altın(KG/TL)        Gümüş(KG/TL)        Platin(KG/TL)       Paladyum(KG/TL) ");
+                sayac = 0;
+              
+            }
             List<Deger> list = new List<Deger>();
             XDocument xDocument = XDocument.Load("https://www.borsaistanbul.com/datfile/kmtpkpnsxml");
             list = xDocument.Descendants("TL").Select(o => new Deger
@@ -26,15 +34,15 @@ namespace Odev
 
             }).ToList();
 
-           
-            Console.Write((list[0].Altin) + "  " + (list[0].Gumus) + "  " + (list[0].Platin) + "  " + (list[0].Paladyum) +"\n");
+
+            Console.Write((list[0].Altin) + "         " + (list[0].Gumus) + "         " + (list[0].Platin) + "         " + (list[0].Paladyum) +  "  ===>  " + DateTime.Now + "  Güncellendi.\n");
 
             TextWriter Writer = new StreamWriter("MetalFiyat.text");
-            Writer.WriteLine("altın       gümüş     platin      paladyum ");
-            Writer.WriteLine((list[0].Altin) + "  " + (list[0].Gumus) + "  " + (list[0].Platin) + "  " + (list[0].Paladyum) );
+            Writer.WriteLine("Altın(KG/TL)        Gümüş(KG/TL)      Platin(KG/TL)       Paladyum(KG/TL)  ");
+            Writer.WriteLine((list[0].Altin) + "          " + (list[0].Gumus) + "          " + (list[0].Platin) + "          " + (list[0].Paladyum) + "    ===>    " + DateTime.Now + "  Güncellendi.");
             Writer.Close();
-            Thread.Sleep(1000);
-           
+            Thread.Sleep(60000);
+            sayac = 1 + sayac;
                 goto A;
             
            
